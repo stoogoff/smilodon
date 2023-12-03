6deee7a009a66beb9401f1f68166a05250d4d447
Stoo <stoo.goff@gmail.comn>
2023-12-03 16:28:01 +0000

    Added test seed data and a means for inserting it

--- plugins/client.js
+++ plugins/client.js
@@ -4,10 +4,19 @@ import entity from '~/state/entity'
 import category from '~/state/category'
 import project from '~/state/project'
 
+// seed data
+import projectData from '~/static/seed/projects'
+import categoryData from '~/static/seed/categories'
+import entityData from '~/static/seed/entities'
+
 export default ({ env }, inject) => {
 	const database = new PouchDB(env.database)
 
 	inject('entities', entity(database))
 	inject('categories', category(database))
 	inject('projects', project(database))
+
+	database.bulkDocs(projectData)
+	database.bulkDocs(categoryData)
+	database.bulkDocs(entityData)
 }
