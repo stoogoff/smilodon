a93b1ae43c0d9d9aeaf19da772e335a7edf27518
Stoo <stoo.goff@gmail.comn>
2024-07-08 23:33:36 +0100

    Show warning if an entity has unsaved changes

--- components/entities/entity-item.vue
+++ components/entities/entity-item.vue
@@ -3,6 +3,7 @@
 		<nuxt-link :to="entity.slug">
 			<entity-icon :entity="entity" :class="{ 'text-primary': isActive }" />
 			<span class="hover:link hover:link-primary">{{ entity.title }}</span>
+			<icon-view icon="alert" class="text-error" v-if="hasUnsavedChanges" />
 		</nuxt-link>
 	</li>
 </template>
@@ -10,6 +11,7 @@
 
 import Vue from 'vue'
 import { PROJECT_ID_PREFIX } from '~/utils/config'
+import { local } from '~/utils/storage'
 
 export default Vue.component('EntityItem', {
 	props: {
@@ -22,6 +24,10 @@ export default Vue.component('EntityItem', {
 	computed: {
 		isActive() {
 			return this.$route.fullPath === this.entity.slug
+		},
+
+		hasUnsavedChanges() {
+			return this.entity ? local.has(this.entity._id) : false
 		},
 	},
 })
