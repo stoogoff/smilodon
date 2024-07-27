104d7eaa898cad117491c05cd1caaf62db7120d2
Stoo <stoo.goff@gmail.com>
2024-07-27 12:29:04 +0100

    Improved link popup

--- src/components/editor/link-button.vue
+++ src/components/editor/link-button.vue
@@ -5,22 +5,28 @@
 				<icon-view icon="link" />
 			</d-tooltip>
 		</d-button>
-		<confirm-dialogue
-			v-if="showLinkModal"
-			title="Link"
-			show-cancel
-			@cancel="closeLinkModal"
-			@confirm="createLink"
-		>
-			<d-validator-control
-				label="URL"
-				:value="url"
-				:rules="rules.url"
-				v-slot="{ error }"
-			>
-				<d-input v-model="url" bordered sm :error="error" />
-			</d-validator-control>
-		</confirm-dialogue>
+		<d-card v-show="showLinkModal" compact class="link-modal w-96 absolute z-10" ref="modal">
+			<div class="flex gap-2">
+				<div class="flex-1">
+					<d-validator-control
+						label="URL"
+						:value="url"
+						:rules="rules.url"
+						v-slot="{ error }"
+					>
+						<d-input v-model="url" bordered sm :error="error" />
+					</d-validator-control>
+				</div>
+				<div class="flex-initial">
+					<d-button sm ghost class="mt-9" @click="createLink">
+						<icon-view icon="edit" sm />
+					</d-button>
+					<d-button sm ghost class="mt-9" @click="closeLinkModal">
+						<icon-view icon="close" sm />
+					</d-button>
+				</div>
+			</div>
+		</d-card>
 	</span>
 </template>
 <script>
@@ -51,7 +57,22 @@ export default Vue.component('LinkButton', {
 			showLinkModal: false,
 			url: '',
 			editingData: null,
+			suppressClose: false,
 		}
+	},
+
+	mounted() {
+		document.addEventListener('click', (event) => {
+			if(notNull(event.target.closest('.link-modal'))) return
+
+			Vue.nextTick(() => {
+				if(!this.suppressClose) {
+					this.showLinkModal = false
+				}
+
+				this.suppressClose = false
+			})
+		})
 	},
 
 	computed: {
@@ -64,28 +85,53 @@ export default Vue.component('LinkButton', {
 
 	methods: {
 		closeLinkModal() {
-			this.url = ''
+			this.url = '' // TODO this causes the form to error, messages need to be reset
 			this.showLinkModal = false
 			this.editingData = null
 		},
 
 		openLinkModal() {
 			this.editingData = null
+			this.suppressClose = true
 
-			const anchor = this.editor.selection.$anchor
-			const node = anchor.doc.nodeAt(anchor.pos)
+			const { $anchor } = this.editor.selection
+			const node = $anchor.doc.nodeAt($anchor.pos)
 			const linkMarks = node.marks.filter(m => m.type.name === MARKS.LINK)
+			const nodeStart = $anchor.pos - $anchor.textOffset
 
+			// the selection is on a link, set data needed for update
 			if(linkMarks.length > 0) {
+
 				this.url = linkMarks[0].attrs.href || ''
 				this.editingData = {
-					from: anchor.pos - anchor.textOffset,
-					to: anchor.pos - anchor.textOffset + node.nodeSize,
+					// start position of the link node
+					from: nodeStart,
+					// end position of the link node (basically, text  range)
+					to: nodeStart + node.nodeSize,
+					// the type of mark to be created
 					markType: linkMarks[0].type,
 				}
 			}
 
 			this.showLinkModal = true
+
+			Vue.nextTick(() => {
+				// set the position of the modal
+				const startPos = this.editor.view.coordsAtPos(nodeStart)
+				const { $el } = this.$refs.modal
+				const offsetBox = $el.offsetParent.getBoundingClientRect()
+				const left = startPos.left - offsetBox.left
+
+				$el.style.left = left + 'px'
+				$el.style.top = (startPos.bottom - offsetBox.top + 2) + 'px'
+
+				// if the modal right edge is off screen reposition
+				const elBox = $el.getBoundingClientRect()
+
+				if(elBox.right > offsetBox.right) {
+					$el.style.left = (left - (elBox.right - offsetBox.right - 5)) + 'px'
+				}
+			})
 		},
 
 		createLink() {
